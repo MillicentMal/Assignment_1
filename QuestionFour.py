1e3555bfe1c216b4438b41ad354b8c73a3f700ac
MillicentMal <71253520+MillicentMal@users.noreply.github.com>
2022-02-16 16:02:39 +0200

    first working solution for undo list

--- QuestionFour.py
+++ QuestionFour.py
@@ -1,53 +1,146 @@
 # implementation of Undoable ordered list
+
 class Node:
+    """Node with prev and next pointers
+    """
     def __init__(self, value):
         self.value = value
         self.prev = None
         self.next = None
-    
-    def getValue(self):
-        return self.value
-    
-    def __repr__(self):
-        return self.getValue()
-    
-    def __str__(self):
-        return self.getValue()
+
 
 class OrderedList:
-    def __init__(self, value=None):
-        self.head = value
+    """
+    Ordered list that takes in a value and inserts it in a position such that it is ordered
+    """
+    def __init__(self):
+        self.head = None
+        self.undoStack = []
+        self.redoStack = []
 
+    def insert(self, value):
+        """Insets value into the list in an orderly manner
+        if list is [1, 2, 3] and value is insert calling self.insert(-1) will give the list as  [-1, 1, 2, 3]
+        
 
-    def add(self, value):
-        stop = False
-        newNode = Node(value)
-        if self.head == None:
-            self.head = newNode
+        Args:
+            value (_int_): _a value to insert into the ordered list_
             
-        currentNode = self.head
-        while currentNode.next is not None and not stop:
-            if currentNode.getValue() > value:
+        """
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.value > value:
                 stop = True
             else:
-                previous = currentNode
-                currentNode = currentNode.next
-        if currentNode is None:
-            newNode.next = self.head
+                previous = current
+                current = current.next
+
+        temp = Node(value)
+        if previous == None:
+            temp.next = self.head
+            self.head = temp
         else:
-            newNode.next = currentNode
-            
+            temp.next = current
+            previous.next = temp
+            # inserting the method into our undoStack so that we can easily pop items
+        undoItem  = ('insert', value)
+        self.undoStack.append(undoItem)
         
-    def getItems(self):
-        currentNode = self.head
-        while currentNode.next is not None:
-            currentNode.getValue()
-            currentNode = currentNode.next
-                    
+    
+    def print(self):
+        # to print a list representation of our LinkedList
+        ordered = []
+        if self.head == None:
+            return None
+        else:
+            current = self.head
+            while current != None:
+                ordered.append(current.value)
+                current = current.next
+        return ordered
+    
+    def delete(self, value):
+        """Deletes value
+
+        Args:
+            value (int): is deleted if found
+
+        Raises:
+            ValueError: If value is not found
+        """
+        try:
+            if self.head == None:
+                return
+            elif self.head.value == value:
+                self.prevVersion = self.print()
+                self.head.prev = None
+                self.head = self.head.next
+                return
+            current = self.head
+            while current.next:
+                if current.next.value == value:
+                    current.next = current.next.next
+                    undoItem  = ('delete', value)
+                    self.undoStack.append(undoItem)
+                    return self.prevVersion
+                current = current.next
+        except ValueError:
+            return "value not in list"
+    
 
     
-first = OrderedList()
-first.add(2)
-first.add(12)
-first.add(1)
-print(first.getItems())
+    def undo(self):
+        """Undo undoes the last operation on our list
+
+        Returns:
+        : None 
+        """
+        if len(self.undoStack) == 0:
+            return "Nothing to undo"
+        toUnDo = self.undoStack[-1]        
+        if toUnDo[0] == 'insert':
+            print(toUnDo[1])
+            self.delete(toUnDo[1])
+            item = self.undoStack.pop()
+            self.redoStack.append(item)
+        elif toUnDo[0] == 'delete':
+            self.insert(toUnDo[1])
+            item = self.undoStack.pop()
+            self.redoStack.append(item)
+    
+    def redo(self):
+        """Redoes the operation that was cancelled by undo
+
+        Returns:
+            None_
+        """
+        if len(self.redoStack) == 0:
+            return "Nothing to redo"
+        toReDo = self.redoStack[-1]
+        if toReDo[0] == 'insert':
+            self.insert(toReDo[1])
+        elif toReDo[0] == 'delete':
+            self.delete(toReDo[1])
+        self.redoStack.pop()
+        
+             
+newOne = OrderedList()
+newOne.insert(3)
+newOne.insert(5)
+newOne.insert(1)
+print(newOne.print())
+print(newOne.undoStack)
+print("Calling undo")
+newOne.undo()
+print(newOne.print())
+print(newOne.undoStack)
+print("Calling undo")
+newOne.undo()
+print(newOne.print())
+print(newOne.undoStack)
+print("Calling undo")
+newOne.undo()
+print(newOne.print())
+print(newOne.undoStack)
